1ebe6872ba3f198cd662ae41cbf1671b347378d1
Murat Ülker <muratulker93@gmail.com>
2022-10-19 08:21:27 +0300

    Revert "Merge branch 'main' of https://github.com/ulkermu/denizalti"
    This reverts commit 37d7f3de8bfb80561a906d407ce9bfa6495cc6f7, reversing
    changes made to 73cf35c04552942796cfc7d628d10fd306dee9b3.

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,2 @@
 // Instantiating the global app object
 var app = {};
-
-//* Toggle Nav Bar
-
-const menuLink = document.querySelector(".nav-bar-burger");
-const menuOverlay = document.querySelector(".menu-overlay");
-const burger1 = document.querySelector(".nav-bar-burger-inc-1");
-const burger2 = document.querySelector(".nav-bar-burger-inc-2");
-const burger3 = document.querySelector(".nav-bar-burger-inc-3");
-
-menuLink.addEventListener("click", () => {
-  menuOverlay.classList.toggle("open");
-  burger1.classList.toggle("nav-bar-burger-inc");
-  burger2.classList.toggle("nav-bar-burger-inc");
-  burger3.classList.toggle("nav-bar-burger-inc");
-  document.body.classList.toggle("hide-scrollbar");
-});
-
-window.addEventListener("resize", () => {
-  if(window.innerWidth > 1201) {
-    document.body.classList.remove("hide-scrollbar");
-  } else if (window.innerWidth < 1201 && menuOverlay.classList.contains('open')) {
-    document.body.classList.add("hide-scrollbar");
-  } else if (window.innerWidth < 1201 && !menuOverlay.classList.contains('open')) {
-    document.body.classList.remove("hide-scrollbar");
-  }
-});
